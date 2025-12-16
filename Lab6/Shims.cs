9e827b5cf62e3dfcadfd7b0967ac817dc2e41169
Ramy Berrekia <ramyberrekia@gmail.com>
2025-12-16 23:01:35 +0300

    Create Shims.cs

---
+++ Lab6/Shims.cs
@@ -0,0 +1,13 @@
+namespace Lab6
+{
+    public delegate void Sorting(int[] row);
+
+    public class Finder { }
+    public class SortRowsStyle { }
+    public class ReplaceMaxElements { }
+    public class BikeRide { }
+    public class GetTriangle { }
+    public class SortRowsByMax { }
+    public class FindNegatives { }
+    public class MathInfo { }
+}
